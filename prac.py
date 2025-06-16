169b780a5920028989b591e139a52a3919ce9aba
milan777777 <milangthapa07@gmail.com>
2025-06-16 13:07:48 +0545

    prc

--- prac.py
+++ prac.py
@@ -16,6 +16,7 @@ class School:
         self.grade = {'course_name': 'grade'}
         self.absent = False
         self.house = set()
+        self.__salary = {}
 
     def add_grade(self, course_name, grade):
         self.grade[course_name] = grade
@@ -26,21 +27,41 @@ class School:
         print(self.sports)
 
     def mark_absent(self, status):
-        self.absent = status
+        self.absent = bool(status)
         print("Result:", self.absent)
 
     def add_house(self, house_name):
         self.house.add(house_name)
         print("house:", self.house)
 
+    def get_salary(self, new_salary):
+        self.__salary = new_salary
+
     def show_boy_girl_ratio(self, boys, girls):  
         ratio = calculate_boy_girl_ratio(boys, girls)
         print("Boy-Girl Ratio:", ratio)
 
-sch = School("10", "100", "Triiii")
+class PublicSchool(School):
+    def __init__(self, teachers, students, school_name, area,funding):
+        super().__init__(teachers, students, school_name, area)
+        self.funding = funding
+        self. uniform_color = "blue"
+
+    def show_funding(self):
+        print (f"School funding is from {self.funding}")
+
+    def show_uniform_color(self):
+        print(f"the uniform color is {self.uniform_color}")
+
+
+sch = PublicSchool("10", "100", "Triiii", 25, "Government")
 teacher_message(sch.school_name)
 sch.add_grade("Maths", "A")
 sch.add_sports("football", "forward")
 sch.mark_absent(False)
 sch.add_house("Green")
+print(calculate_boy_girl_ratio(60,50))
 sch.show_boy_girl_ratio(60,50)
+sch.show_funding()
+sch.show_uniform_color()
+sch.get_salary("private")
